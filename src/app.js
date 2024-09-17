313fa8836ee8e835f677b632d1008e9e58d7deb1
SarahOtmane <sarahotmane02@gmail.com>
2024-09-17 11:26:01 +0200

    remove mongoose.connect

--- src/app.js
+++ src/app.js
@@ -3,12 +3,9 @@ const app = express()
 const port = 3000;
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./docs/swagger-config.js');
+const swaggerSpec = require('./doc/swagger-config.js');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const mongoose = require("mongoose");
-
-// mongoose.connect('mongodb://127.0.0.1:27017/apiSecretSanta');
 
 app.use(express.urlencoded());
 app.use(express.json());
