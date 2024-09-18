0c49a21ef03c9d97cd4a0def9133af8017415d69
SarahOtmane <sarahotmane02@gmail.com>
2024-09-18 18:15:46 +0200

    update function

--- src/routes/accessoryRoute.js
+++ src/routes/accessoryRoute.js
@@ -3,6 +3,6 @@ module.exports = (server) => {
     const accessoryController = require('../controllers/accessoryController');
 
     server.post('/accessories', accessoryController.createAnAccessory);
-    server.get('/accessories', accessoryController.getAccessories);
+    server.get('/accessories', accessoryController.getAllAccessory);
     server.get('/accessories/:name', accessoryController.getAnAccessory);
 }
