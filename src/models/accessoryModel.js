4dee13d2aee9825e1dcc8206e8bdbd264802ed4e
SarahOtmane <sarahotmane02@gmail.com>
2024-09-18 10:51:08 +0200

    update name of the collection

--- src/models/accessoryModel.js
+++ src/models/accessoryModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let userSchema = new Schema({
+let accessorySchema = new Schema({
     name:{
         type : String,
         required : true,
@@ -17,4 +17,4 @@ let userSchema = new Schema({
     },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Accessory', accessorySchema);
