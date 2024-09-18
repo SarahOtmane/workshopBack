4b92ff150024bfa12d54633cc9c3b81a378236e2
SarahOtmane <sarahotmane02@gmail.com>
2024-09-18 11:17:36 +0200

    update la connexion a la base de donnée

--- src/app.js
+++ src/app.js
@@ -6,8 +6,15 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./doc/swagger-config.js');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/workshop1');
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db:27017/${process.env.DB_NAME}?authSource=admin`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
 
 
 app.use(express.urlencoded());
