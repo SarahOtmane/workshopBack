4b1c4826420378a52807ef5fda1c4f82aa1872ef
SarahOtmane <sarahotmane02@gmail.com>
2024-09-18 10:41:42 +0200

    connect  to database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()  
-const port = 3000;
+const port = 3030;
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./doc/swagger-config.js');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/workshop1');
 
 
 app.use(express.urlencoded());
