03429bd7e384188460fd9852423133d461087dd2
SarahOtmane <sarahotmane02@gmail.com>
2024-09-19 10:28:56 +0200

    update variables

--- src/services/connectDB.js
+++ src/services/connectDB.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
     const isTestEnv = process.env.NODE_ENV === 'test';
 
     // Choisir les bonnes variables selon l'environnement
-    const dbUser = isTestEnv ? process.env.TEST_DB_USER : process.env.DEV_DB_USER;
-    const dbPassword = isTestEnv ? process.env.TEST_DB_PASSWORD : process.env.DEV_DB_PASSWORD;
-    const dbName = isTestEnv ? process.env.TEST_DB_NAME : process.env.DEV_DB_NAME;
+    const dbUser = isTestEnv ? process.env.DB_USER_TEST: process.env.DB_USER;
+    const dbPassword = isTestEnv ? process.env.DB_PASSWORD_TEST : process.env.DB_PASSWORD;
+    const dbName = isTestEnv ? process.env.DB_NAME_TEST : process.env.DB_NAME;
 
     try {
         await mongoose.connect(`mongodb://${dbUser}:${dbPassword}@db:27017/${dbName}?authSource=admin`, {
