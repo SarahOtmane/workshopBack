178b60c687981402b1eae095e6641d5d34a61a12
SarahOtmane <sarahotmane02@gmail.com>
2024-09-19 21:15:38 +0200

    onfig route of registerAUser

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,4 +2,5 @@ module.exports = (server) => {
     const userController = require('../controllers/userController');
 
     server.post('/users/login', userController.loginAUser);
+    server.post('/users/register', userController.registerAUser);
 }
