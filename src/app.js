f52800ae82d8b367e45f06a7e2fa2ae951d6ea45
SarahOtmane <sarahotmane02@gmail.com>
2024-09-19 10:05:57 +0200

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,19 @@
-const express = require('express')
-const app = express()  
+const express = require('express');
+const app = express();
 const port = 3030;
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./doc/swagger-config.js');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Load environment variables
+require('dotenv').config();
 
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db:27017/${process.env.DB_NAME}?authSource=admin`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log(`Connected to MongoDB ${process.env.DB_NAME}`);
-}).catch(err => {
-    console.error('MongoDB connection error:', err);
+// Import and connect to the database
+const connectDB = require('./db.js');
+connectDB();
+
+// Configure routes and middleware
+const configureServices = require('./service-config.js');
+configureServices(app);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
-
-app.use(express.urlencoded());
-app.use(express.json());
-
-const userRoute = require('./routes/userRoute.js');
-const accessoryRoute = require('./routes/accessoryRoute.js');
-const productController = require('./routes/productRoute.js');
-
-userRoute(app);
-accessoryRoute(app);
-productController(app);
-
-app.listen(port, () =>{
-    console.log(`Example app listenning on port ${port}`);
-})
