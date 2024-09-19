f7619f47ffedf33155fe263a207e88e8600e31ec
SarahOtmane <sarahotmane02@gmail.com>
2024-09-19 21:14:12 +0200

    add function registerAUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,3 +53,42 @@ exports.loginAUser = async(req, res) =>{
         res.status(500).json({message: 'Une erreur s\'est produite lors du traitement'});
     }
 }
+
+
+
+exports.registerAUser = async(req, res) =>{
+    try {
+        const { email, password } = req.body;
+
+        // Check for missing fields
+        if (!email || !password) {
+            return res.status(403).json({ message: 'Tous les champs sont requis.' });
+        }
+
+        // Check for valid email format
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(403).json({ message: 'Format d\'email invalide.' });
+        }
+
+        // Vérification de l'existence de l'email
+        const existingEmail = await User.findOne({ where: { email } });
+        if (existingEmail) {
+            return res.status(409).json({ message: 'Cet email existe déjà.' });
+        }
+
+        // Hachage du mot de passe
+        const hashedPassword = await argon2.hash(password);
+
+        // Création de l'utilisateur
+        await User.create({
+            email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json({ message: `Utilisateur créé avec succès.`});
+    } 
+    catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la création de l\'utilisateur.' });
+    }
+};
